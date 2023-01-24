b27073fe1d4be9fa562896d81533e8022585be2c
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-24 09:34:55 -0400

    add: show the variable in the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
             alt="camera-logo">        
         </div>
         <div class="col-9 p-5">
-            <div><h1>FreeCodeCamp</h1></div>
+            <div><h1>{{ $user -> username }}</h1></div>
             <div class="d-flex">
                 <div class="pe-5"><strong>153</strong> posts</div>
                 <div class="pe-5"><strong>23k</strong> followers</div>
