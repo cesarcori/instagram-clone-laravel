1ad64669db5ec8e3d442313af767eee6b8882ee3
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 12:00:34 -0400

    add: field Post Caption

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,5 +2,23 @@
 
 @section('content')
 <div class="container">
+  <div class="row">
+    <div class="col-8 offset-2">
+        <div class="row mb-3">
+            <label for="caption" class="col-md-4 col-form-label text-md-end">Post Caption</label>
+
+            <div class="col-md-6">
+                <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" caption="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+
+                @error('caption')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+    
+    </div>
+  </div>
 </div>
 @endsection
