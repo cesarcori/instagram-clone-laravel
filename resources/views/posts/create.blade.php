1cb5098ec91cb79a01996efd5f7d1b85372a5cc1
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 17:32:07 -0400

    add: button

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,38 +2,45 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-8 offset-2">
-        <div class="row mb-3">
-            <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
-
-            <input id="caption" 
-                type="text" 
-                class="form-control @error('caption') is-invalid @enderror" 
-                caption="caption" 
-                value="{{ old('caption') }}" 
-                required 
-                autocomplete="caption" 
-                autofocus>
-
-            @error('caption')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
-
+    <form action="">
         <div class="row">
-            <label for="image" class="col-md-4 col-form-label">Post Image</label>
-            <input type="file" class="form-control-file">
-            @error('image')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+            <div class="col-8 offset-2">
 
-    </div>
-  </div>
+                <div class="row mb-3">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+
+                    <input id="caption" 
+                        type="text" 
+                        class="form-control @error('caption') is-invalid @enderror" 
+                        caption="caption" 
+                        value="{{ old('caption') }}" 
+                        required 
+                        autocomplete="caption" 
+                        autofocus>
+
+                    @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class="form-control-file">
+                    @error('image')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+
+            </div>
+        </div>
+    </form>
 </div>
 @endsection
