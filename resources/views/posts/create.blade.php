1e2dbd264e3414d556a333142617f0e4bd8398c7
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 18:01:01 -0400

    fix: required take out

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -19,7 +19,6 @@
                         class="form-control @error('caption') is-invalid @enderror" 
                         name="caption"
                         value="{{ old('caption') }}" 
-                        required 
                         autocomplete="caption" 
                         autofocus>
 
@@ -34,9 +33,7 @@
                     <label for="image" class="col-md-4 col-form-label">Post Image</label>
                     <input type="file" class="form-control-file" id="image" name="image">
                     @error('image')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                        <strong>{{ $message }}</strong>
                     @enderror
                 </div>
                 
