5dd5752c7ee0ac7aad9f7ec843f795d8b395ba1e
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 17:52:28 -0400

    add: csrf secure

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
         <div class="row">
             <div class="col-8 offset-2">
 
@@ -16,7 +17,7 @@
                     <input id="caption" 
                         type="text" 
                         class="form-control @error('caption') is-invalid @enderror" 
-                        caption="caption" 
+                        name="caption"
                         value="{{ old('caption') }}" 
                         required 
                         autocomplete="caption" 
@@ -31,7 +32,7 @@
 
                 <div class="row">
                     <label for="image" class="col-md-4 col-form-label">Post Image</label>
-                    <input type="file" class="form-control-file">
+                    <input type="file" class="form-control-file" id="image" name="image">
                     @error('image')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
