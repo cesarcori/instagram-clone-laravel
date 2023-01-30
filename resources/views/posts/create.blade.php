56dbbddc3b9ad129e35673c4c307391dfc112fd2
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 17:20:41 -0400

    add: upload image

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -22,6 +22,17 @@
                 </span>
             @enderror
         </div>
+
+        <div class="row">
+            <label for="image" class="col-md-4 col-form-label">Post Image</label>
+            <input type="file" class="form-control-file">
+            @error('image')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
     </div>
   </div>
 </div>
