8d584361b24471d16cbd836c2f8ac9039027486d
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 17:51:52 -0400

    add: store route

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,6 @@ Auth::routes();
 
 //Route::get('/p', 'PostsController@create');
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
+Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
