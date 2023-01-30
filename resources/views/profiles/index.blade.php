17a2013fbb6340a98ac8c36847d77369586cac6e
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-30 11:35:30 -0400

    change name home to index

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://www.seekpng.com/png/small/52-526178_logo-clipart-camera-camera-png-for-logo.png" 
+            style="height: 100px;"
+            class="rounded-circle"
+            alt="camera-logo">        
+        </div>
+        <div class="col-9 p-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user -> username }}</h1>
+                <a href="#">Add new post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> posts</div>
+                <div class="pe-5"><strong>23k</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-2 fw-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>   ,
+        </div>
+    </div>
+    <div class="row p-5">
+        <div class="col-4">
+            <img src="https://discourse-user-assets.s3.amazonaws.com/original/2X/f/f9165819dd1e91941cd3c7481d28adba04c9ccaf.png" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://pathrise-website-guide-wp.s3.us-west-1.amazonaws.com/guides/wp-content/uploads/2019/03/10183927/danial-ricaros-785707-unsplash.jpeg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://pathrise-website-guide-wp.s3.us-west-1.amazonaws.com/guides/wp-content/uploads/2019/09/26230659/mimi-thian-5ZnS3wK6sUg-unsplash.jpeg" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
