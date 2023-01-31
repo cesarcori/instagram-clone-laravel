60030d9eb8d1e108b755d9d760b064b63043eba8
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-01-31 11:49:45 -0400

    add: show the post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -39,4 +39,10 @@ class PostsController extends Controller
 
         return redirect('/profile/' . auth()->user()->id);
     }
+
+    public function show(\App\Models\Post $post)
+    {
+        //dd($post);
+        return view('posts.show', compact('post'));
+    }
 }
