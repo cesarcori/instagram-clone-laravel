6c5287ddeb97e0416e247093a72854839479df5b
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-02-11 13:17:20 -0400

    add: username in show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,11 @@
         <div class="col-8">
             <img src="/storage/{{ $post->image }}" class="w-100">
         </div>
+        <div class="col-4">
+            <div>
+                <h3>{{ $post->user->username }}</h3>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
