180778488627cea6b0f23cba0f365dcd78620274
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-02-14 21:17:03 -0400

    add: add route url of edit

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create'])
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
