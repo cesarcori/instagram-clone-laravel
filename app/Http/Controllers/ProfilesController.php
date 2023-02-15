72a0c06c3b421fd004b0c2a7c8829b2666abc013
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-02-14 21:18:15 -0400

    add: function controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,13 +7,19 @@ use App\Models\User;
 
 class ProfilesController extends Controller
 {
-    public function index($user)
+    public function index(User $user)
     {
         /*dd(User::find($user));*/
-        $user = User::findOrFail($user);
+        //$user = User::findOrFail($user);
 
-        return view('profiles.index', [
-            'user' => $user,
-        ]);
+        //return view('profiles.index', [
+            //'user' => $user,
+        //]);
+        return view('profiles.index', compact('user'))
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'))
     }
 }
