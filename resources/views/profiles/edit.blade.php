e8dc849db7bcbda3adac93c6433e7335e167e5b9
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-02-25 21:01:36 -0400

    add: fields description and url

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -13,7 +13,7 @@
                 </div>
 
                 <div class="row mb-3">
-                    <label for="title" class="col-md-4 col-form-label">Post Title</label>
+                    <label for="title" class="col-md-4 col-form-label">Title</label>
 
                     <input id="title" 
                         type="text" 
@@ -24,6 +24,42 @@
                         autofocus>
 
                     @error('title')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="row mb-3">
+                    <label for="description" class="col-md-4 col-form-label">Description</label>
+
+                    <input id="description" 
+                        type="text" 
+                        class="form-control @error('description') is-invalid @enderror" 
+                        name="description"
+                        value="{{ old('description') }}" 
+                        autocomplete="description" 
+                        autofocus>
+
+                    @error('description')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="row mb-3">
+                    <label for="url" class="col-md-4 col-form-label">Url</label>
+
+                    <input id="url" 
+                        type="text" 
+                        class="form-control @error('url') is-invalid @enderror" 
+                        name="url"
+                        value="{{ old('url') }}" 
+                        autocomplete="url" 
+                        autofocus>
+
+                    @error('url')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
