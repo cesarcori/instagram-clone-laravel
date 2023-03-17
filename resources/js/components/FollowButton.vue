4a330ba794a9805f3164b79204546fe83d6e4efd
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-03-17 16:37:54 -0400

    Add: component follow button

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+        <button class="btn btn-primary ms-4">Follow something</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
