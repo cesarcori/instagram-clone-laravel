87454ea15eac81f9f33db48ebdaac7c2f7633723
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-03-18 12:12:05 -0400

    add: function followers to stablish relation ship with User

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+
 class Profile extends Model
 {
     use HasFactory;
@@ -15,6 +17,11 @@ class Profile extends Model
     {
         $imagePath = ($this->image) ? $this->image : 'profile/DH9M36mP9wJs4kb5QpJmWq50jgfb9soaUe0g3npd.png'; 
         return '/storage/' . $imagePath ;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
     }
 
     public function user(){
