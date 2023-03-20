7fc9d7aa34915ee1bcf240c5443bfd1c7815b868
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-03-20 18:37:01 -0400

    order images and link to profile

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,25 +4,15 @@
 <div class="container">
     @foreach($posts as $post)
         <div class="row">
-            <div class="col-8">
-                <img src="/storage/{{ $post->image }}" class="w-100">
+            <div class="col-8 offset-2">
+                <a href="/profile/{{ $post->user->id }}">
+                    <img src="/storage/{{ $post->image }}" class="w-100">
+                </a>
             </div>
-            <div class="col-4">
+        </div>
+        <div class="row pt-2 pb-4">
+            <div class="col-8 offset-2">
                 <div>
-                    <div class="d-flex align-items-center">
-                        <div class="pe-3">
-                            <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 40px">
-                        </div>
-                        <div>
-                            <div class="fw-bold">
-                                <a href="/profile/{{ $post->user->id }}">
-                                    <span class="text-dark">{{ $post->user->username }}</span>
-                                </a>
-                                <a href="#" class="ps-3">Follow</a>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
                     <p>
                         <span class="fw-bold">
                             <a href="/profile/{{ $post->user->id }}">
