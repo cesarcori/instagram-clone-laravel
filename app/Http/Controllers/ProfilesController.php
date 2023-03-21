9bf213fc8226b0c07a133c8c4b163b6828801762
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-03-21 13:20:34 -0400

    add: Cache to variables counts.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use App\Models\User;
 use Intervention\Image\Facades\Image;
+
 
 class ProfilesController extends Controller
 {
@@ -17,9 +19,25 @@ class ProfilesController extends Controller
             //'user' => $user,
         //]);
         
-        $postCount = $user->posts->count();
-        $followersCount = $user->profile->followers->count();
-        $followingCount = $user->following->count();
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id, 
+            now()->addSeconds(30), 
+            function() use($user) {
+                return $user->posts->count();
+            });
+        $followersCount =  Cache::remember(
+            'count.followers.' . $user->id, 
+            now()->addSeconds(30), 
+            function() use($user) {
+                return $user->profile->followers->count();
+            }); 
+            
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id, 
+            now()->addSeconds(30), 
+            function() use($user) {
+                return $user->following->count();
+            });             
 
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
